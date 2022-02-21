dc85a4865b82ed7ce01dcce00014565f5ab79964
PC Oficina Carlos <78969981+Carloxer@users.noreply.github.com>
2022-02-21 11:48:38 -0400

    Actualización código AutoConnect

---
+++ Prueba de codigo/AutoConnect/AutoConnect.ino
@@ -0,0 +1,95 @@
+#include <WiFi.h>          
+#include <WebServer.h>     
+#include <AutoConnect.h>
+#include <AutoConnectCredential.h>
+
+WebServer         Server;          
+AutoConnect       Portal(Server);
+AutoConnectConfig config("Tesis_AP","kuma2018");      //Credenciales para acceder al AP
+
+//Declaración de elementos AutoConnect para la página web de Configuración del AP
+ACText(caption, "     Desde este portal podrá cambiar el nombre (SSID) y la contraseña del punto de acceso", "text-align:justify;font-family:serif;color:#000000;");
+ACText(header, "<h2>Cambiar Credenciales</h2>", "text-align:center;color:2f4f4f;");
+ACInput(ssid, "", "Nueva SSID", "", "Introduzca su nuevo SSID");
+ACInput(pass1, "", "Nueva Clave", "", "Introduzca su nueva clave");
+ACInput(pass2, "", "Confirme Clave", "", "Introduzca su clave de nuevo");
+ACSubmit(change, "Guardar cambios", "/_ac");
+ACSubmit(back, "Volver", "/_ac");
+
+//Declaración de la página web para la página web de Configuración del AP
+AutoConnectAux ap_config("/ap_config", "Configuración de AP", true,{
+  caption,
+  header,
+  ssid,
+  pass1,
+  pass2,
+  change,
+  back,
+});
+
+//Función para mostrar la página de inicio al conectar
+void rootPage() {
+  String content = 
+    "<html>"
+    "<head>"
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+    "<title>Tesis_AP</title>"
+    "</head>"
+    "<body>"
+    "<h1 align=\"center\" style=\"color:purple;margin:10px;\">Bienvenido al Menú</h1>"
+    "<p></p>"
+    "<p style=\"padding-top:5px;text-align:center\">"AUTOCONNECT_LINK(COG_24)"</p>"
+    "</body>"
+    "</html>";
+  Server.send(200, "text/html", content);
+}
+
+//Función para borrar las credenciales (sólo para la etapa de desarrollo)
+void deleteAllCredentials(void) {
+  //Se guardan las credenciales que se encuentran en la flash
+  AutoConnectCredential credential;
+  station_config_t      config;
+  uint8_t qty           = credential.entries();       //Cantidad de credenciales guardadas
+
+  Serial.println("Borrando credenciales");
+  if (qty)
+    Serial.printf("Hay %d credenciales guardadas.\n", qty);
+  else {
+    Serial.println("No hay credenciales guardadas.");
+    return;
+  }
+  
+  //Ciclo para borrar las credenciales
+   while (qty--) {
+    credential.load((int8_t)0, &config);                                        //Se cargan las credenciales de la flash
+    if (credential.del((const char*)&config.ssid[0]))                           //Se borran las credenciales cargadas anteriormente
+      Serial.printf("%s fue borrada.\n", (const char*)config.ssid);
+    else
+      Serial.printf("%s NO pudo ser borrada.\n", (const char*)config.ssid);     
+  }                                                
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  deleteAllCredentials();
+  
+  //Se borra la configuración Wi-Fi
+  WiFi.disconnect(true, true);
+
+  config.hostName = "esp32_ap";
+  config.title = "ESP32 Access Point";
+  Portal.config(config);
+  Portal.join({ap_config});
+  Portal.begin();
+  
+  Server.on("/", rootPage);
+  if (Portal.begin()) {
+    Serial.println("WiFi connected: " + WiFi.localIP().toString());
+  }
+}
+
+void loop() {
+    Portal.handleClient();
+}
