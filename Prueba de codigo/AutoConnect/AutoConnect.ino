ca85e04029a8efe9376a91b153c82d05101aeb4a
PC Oficina Carlos <78969981+Carloxer@users.noreply.github.com>
2022-02-22 12:07:05 -0400

    Actualización AutoConnect
    + Datos iniciales /ap_config

--- Prueba de codigo/AutoConnect/AutoConnect.ino
+++ Prueba de codigo/AutoConnect/AutoConnect.ino
@@ -8,13 +8,17 @@ AutoConnect       Portal(Server);
 AutoConnectConfig config("Tesis_AP","kuma2018");      //Credenciales para acceder al AP
 
 //Declaración de elementos AutoConnect para la página web de Configuración del AP
-ACText(caption, "     Desde este portal podrá cambiar el nombre (SSID) y la contraseña del punto de acceso", "text-align:justify;font-family:serif;color:#000000;");
+ACText(caption, "Desde este portal podrá cambiar el nombre (SSID) y la contraseña del punto de acceso", "text-align:justify;font-family:serif;color:#000000;");
 ACText(header, "<h2>Cambiar Credenciales</h2>", "text-align:center;color:2f4f4f;");
 ACInput(ssid, "", "Nueva SSID", "", "Introduzca su nuevo SSID");
 ACInput(pass1, "", "Nueva Clave", "", "Introduzca su nueva clave");
 ACInput(pass2, "", "Confirme Clave", "", "Introduzca su clave de nuevo");
 ACSubmit(change, "Guardar cambios", "/_ac");
 ACSubmit(back, "Volver", "/_ac");
+ACText(note, "<p><b>NOTA:</b> si no desea cambiar algún parámetro, deje la casilla en blanco</p>", "text-align:justify");
+
+//Declaración de elementos AutoConnect para la página web post-configuración del AP
+
 
 //Declaración de la página web para la página web de Configuración del AP
 AutoConnectAux ap_config("/ap_config", "Configuración de AP", true,{
@@ -25,7 +29,16 @@ AutoConnectAux ap_config("/ap_config", "Configuración de AP", true,{
   pass2,
   change,
   back,
+  note,
 });
+
+//Datos iniciales en la página de configuración del AP
+String onConfig(AutoConnectAux& aux, PageArgument& args){
+  aux["ssid"].as<AutoConnectInput>().value = "";
+  aux["pass1"].as<AutoConnectInput>().value = "";
+  aux["pass2"].as<AutoConnectInput>().value = "";
+  return String();
+}
 
 //Función para mostrar la página de inicio al conectar
 void rootPage() {
