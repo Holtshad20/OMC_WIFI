1334290490c05049cd83092c061005b746502b36
PC Oficina Carlos <78969981+Carloxer@users.noreply.github.com>
2022-02-23 12:02:33 -0400

    AutoConnect
    > Modificación portal web

--- Prueba de codigo/AutoConnect/AutoConnect.ino
+++ Prueba de codigo/AutoConnect/AutoConnect.ino
@@ -2,43 +2,67 @@
 #include <WebServer.h>     
 #include <AutoConnect.h>
 #include <AutoConnectCredential.h>
+#include <EEPROM.h>
 
 WebServer         Server;          
 AutoConnect       Portal(Server);
-AutoConnectConfig config("Tesis_AP","kuma2018");      //Credenciales para acceder al AP
+AutoConnectConfig config("OMC_WIFI","12345678");      //Credenciales para acceder al AP
 
 //Declaración de elementos AutoConnect para la página web de Configuración del AP
-ACText(caption, "Desde este portal podrá cambiar el nombre (SSID) y la contraseña del punto de acceso", "text-align:justify;font-family:serif;color:#000000;");
-ACText(header, "<h2>Cambiar Credenciales</h2>", "text-align:center;color:2f4f4f;");
+ACText(caption01, "Desde este portal podrá cambiar el hostname, el SSID y la contraseña del punto de acceso OMC_WIFI", "text-align:justify;font-family:serif;color:#000000;");
+ACText(header01, "<h2>Cambiar Hostname</h2>", "text-align:center;color:2f4f4f;");
+ACInput(hostname, "", "Nuevo Hostname", "", "Introduzca el nuevo Hostname");
+ACText(cond01, "<p>La <b>longitud del Hostname</b> no debe exceder los 64 caracteres, solo puede contener <b>caracteres alfanuméricos</b> y <b>guiones (-)</b> y no puede culminar en un guión (-)</p>", "text-align:justify");
+ACText(rec01, "<p><b>RECOMENDACIÓN:</b> emplear el hostname <u>OMC_WIFI_nombre</u>. Por ejemplo: OMC_WIFI_laptop o OMC_WIFI_minero</p>")
+ACText(header02, "<h2>Cambiar Credenciales</h2>", "text-align:center;color:2f4f4f;");
 ACInput(ssid, "", "Nueva SSID", "", "Introduzca su nuevo SSID");
 ACInput(pass1, "", "Nueva Clave", "", "Introduzca su nueva clave");
 ACInput(pass2, "", "Confirme Clave", "", "Introduzca su clave de nuevo");
-ACSubmit(change, "Guardar cambios", "/_ac");
-ACSubmit(back, "Volver", "/_ac");
-ACText(note, "<p><b>NOTA:</b> si no desea cambiar algún parámetro, deje la casilla en blanco</p>", "text-align:justify");
+ACText(cond02, "<p>La <b>longitud del SSID</b> no debe exceder los 32 caracteres y ,<b>longitud de la clave</b> debe tener entre 8 y 63 caracateres</p>", "text-align:justify");
+ACSubmit(change0, "Guardar cambios", "/post_config");
+ACSubmit(back0, "Volver", "/_ac");
+ACText(note01, "<p><b>NOTA:</b> si no desea cambiar algún parámetro, deje la casilla en blanco</p>", "text-align:justify");
 
 //Declaración de elementos AutoConnect para la página web post-configuración del AP
+
+ACSubmit(reset, "Reiniciar", "/_ac#rdlg");
+ACSubmit(back1, "Volver al menú", "/_ac");
 
 
 //Declaración de la página web para la página web de Configuración del AP
 AutoConnectAux ap_config("/ap_config", "Configuración de AP", true,{
-  caption,
-  header,
+  caption01,
+  header01,
+  hostname,
+  cond01,
+  header02,
   ssid,
   pass1,
   pass2,
-  change,
-  back,
-  note,
+  cond02,
+  change0,
+  back0,
+  note01,
+});
+
+//Declaración de la página web para la página web post-configuración del AP
+AutoConnectAux post_config("/post_config", "Configuración de AP", false,{
+  reset,
+  back1,
 });
 
 //Datos iniciales en la página de configuración del AP
-String onConfig(AutoConnectAux& aux, PageArgument& args){
-  aux["ssid"].as<AutoConnectInput>().value = "";
-  aux["pass1"].as<AutoConnectInput>().value = "";
-  aux["pass2"].as<AutoConnectInput>().value = "";
-  return String();
-}
+//String onConfig(AutoConnectAux& aux, PageArgument& args){
+  //aux["ssid"].as<AutoConnectInput>().value = "";
+  //aux["pass1"].as<AutoConnectInput>().value = "";
+  //aux["pass2"].as<AutoConnectInput>().value = "";
+  //return String();
+//}
+
+//String onPostConfig(AutoConnect& aux, PageArgument& args){
+  
+  
+//}
 
 //Función para mostrar la página de inicio al conectar
 void rootPage() {
@@ -82,6 +106,9 @@ void deleteAllCredentials(void) {
   }                                                
 }
 
+
+//Función para cargar las credenciales de la flash
+
 void setup() {
   delay(1000);
   Serial.begin(115200);
@@ -91,10 +118,11 @@ void setup() {
   //Se borra la configuración Wi-Fi
   WiFi.disconnect(true, true);
 
-  config.hostName = "esp32_ap";
-  config.title = "ESP32 Access Point";
+  config.hostName = "OMC_WIFI";
+  config.title = "OMC_WIFI Access Point";
+  config.homeUri = "/_ac",
   Portal.config(config);
-  Portal.join({ap_config});
+  Portal.join({ap_config, post_config});
   Portal.begin();
   
   Server.on("/", rootPage);
