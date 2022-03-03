6d3e1c1eb87c124717638afb49c8b29f6722e722
Carloxer <78969981+Carloxer@users.noreply.github.com>
2022-03-03 11:57:21 -0400

    Actualización código Relay
    + Inclusión de timer: ocurre una interrupción cada segundo, el programador elige la cantidad de segundos necesarios para volver a permitir el paso de corriente.
    
    >> Se requiere combinar código con medidas analógicas para comprobar buen comportamiento del timer.

--- Prueba de codigo/relay/relay.ino
+++ Prueba de codigo/relay/relay.ino
@@ -1,12 +1,29 @@
 int entero = 0;
 
+volatile int interruptCounter;
+int totalInterruptCounter;
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+
+}
+
 void setup() {
   Serial.begin(115200);       //Inicializa comunicación serial
   delay(100);
 
   pinMode(23, OUTPUT);
-  digitalWrite(23, LOW);
+  digitalWrite(23, HIGH);
   delay(100);
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmEnable(timer);
 }
 
 void loop() {
@@ -17,6 +34,7 @@ void loop() {
 
   char caracteres[6] = "";
 
+  boolean recuperacion = 0;
 
   uint8_t i = 0;
   while (Serial.available() > 0) {
@@ -33,11 +51,28 @@ void loop() {
   }
 
   if (entero <= volt_inf || entero >= volt_sup) {
-    relay = LOW;
-    digitalWrite(23, LOW);
-  }
-  else {
+    recuperacion = 0;
     relay = HIGH;
     digitalWrite(23, HIGH);
+    totalInterruptCounter = 0;
+  }
+  else {
+    recuperacion = 1; 
+    if (totalInterruptCounter >= 10) {
+      relay = LOW;
+      digitalWrite(23, LOW);
+    }
+  }
+
+  if (interruptCounter > 0) {
+    portENTER_CRITICAL(&timerMux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timerMux);
+
+    totalInterruptCounter++;
+    if (recuperacion == 1){
+      Serial.print("Segundos transcurridos desde recuperación: ");
+      Serial.println(totalInterruptCounter);
+    }
   }
 }
