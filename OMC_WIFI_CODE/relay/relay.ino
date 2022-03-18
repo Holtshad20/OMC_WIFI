6dacd6073cbe562b1af1ee4c1d85c36630b7d902
Carloxer <78969981+Carloxer@users.noreply.github.com>
2022-03-18 11:47:32 -0400

    Actualización código Relay
    + Timer FreeRTOS
    
    Nota: comportamiento errático, falta mejorar

--- OMC_WIFI_CODE/relay/relay.ino
+++ OMC_WIFI_CODE/relay/relay.ino
@@ -1,44 +1,43 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+
 boolean relay = LOW;
 
-uint8_t volt_sup = 140;
-uint8_t volt_inf = 100;
+float voltaje = 0;
+float corriente = 7;
 
-boolean recuperacion = 0;
+uint8_t voltSup = 140;
+uint8_t voltInf = 100;
+uint8_t corrSup = 15;
+
+
 boolean controlGlobalRelay = 1;
-
-uint8_t tiempoRecuperacion = 30;
-
 // Si controlGlobalRelay = 0 entonces estamos forzando a que se mantenga apagado sin importar el voltaje o la corriente.
 // Si controlGlobalRelay = 1 entonces estamos trabajando de manera normal con los márgenes de voltaje y corriente normales.
 
-//volatile int interruptCounter;
-//int totalInterruptCounter;
-//hw_timer_t * timer = NULL;
-//portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-//
-//void IRAM_ATTR onTimer() {
-//  portENTER_CRITICAL_ISR(&timerMux);
-//  interruptCounter++;
-//  portEXIT_CRITICAL_ISR(&timerMux);
-//
-//}
+boolean pasoElTiempo = 0;
+uint8_t tiempoRecuperacion = 5;
+
+TimerHandle_t timerRecuperacion;
+
+void pasoTiempoRecuperacion() {
+  pasoElTiempo = 1;
+}
 
 void setup() {
   Serial.begin(115200);       //Inicializa comunicación serial
   delay(100);
 
   pinMode(23, OUTPUT);
-  digitalWrite(23, HIGH);
-  delay(100);
+  digitalWrite(23, LOW);
 
-  //  timer = timerBegin(0, 80, true);
-  //  timerAttachInterrupt(timer, &onTimer, true);
-  //  timerAlarmWrite(timer, 1000000, true);
-  //  timerAlarmEnable(timer);
+  timerRecuperacion = xTimerCreate("TimerDeRecuperacion", pdMS_TO_TICKS(tiempoRecuperacion * 1000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(pasoTiempoRecuperacion));
+  xTimerStop(timerRecuperacion, 0);
+  
+  delay(100);
 }
 
 void loop() {
-  int entero = 0;
   char caracteres[6] = "";
 
   uint8_t i = 0;
@@ -49,37 +48,41 @@ void loop() {
     i++;
 
     if (Serial.available() == 0) {
-      entero = atoi(caracteres);
+      voltaje = atof(caracteres);
       Serial.print("Recibido: ");
-      Serial.println(entero);
+      Serial.println(voltaje);
     }
   }
-
 
   if (controlGlobalRelay == 1) {
-    if ((entero <= volt_inf || entero >= volt_sup ) && relay == LOW) {
-      recuperacion = 0;
-      relay = HIGH;
-      digitalWrite(23, HIGH);
-      tiempoActual = 0;
+    if ( ( (voltaje <= voltInf) || (voltaje >= voltSup) || (corriente >= corrSup) ) && relay == HIGH) {
+      xTimerStop(timerRecuperacion, 0);
+      relay = LOW;
+      digitalWrite(23, LOW);
+
+      Serial.println("Relay apagado");
+      Serial.println("Temporizador detenido");
     }
-    else if (relay == HIGH) {
-      recuperacion = 1;
-      if (tiempoActual >= tiempoRecuperacion) {
-        relay = LOW;
-        digitalWrite(23, LOW);
-        recuperacion = 0;
+    else if (relay == LOW) {
+      if (xTimerIsTimerActive(timerRecuperacion) == pdFALSE ){   
+        xTimerReset(timerRecuperacion, 0);
+        Serial.println();
+        Serial.print("Temporizador inicializado - Esperando ");
+        Serial.print(tiempoRecuperacion);
+        Serial.println(" segundos");
+      }
+      if (pasoElTiempo == 1) {
+        xTimerStop(timerRecuperacion, 0); 
+        relay = HIGH;
+        digitalWrite(23, HIGH);
+        pasoElTiempo = 0;
+        Serial.println("Relay encendido");
+        Serial.println("Temporizador detenido");
       }
     }
-
-    if (recuperacion == 1) {
-      Serial.print("Segundos transcurridos desde recuperación: ");
-      Serial.println(tiempoActual);
-    }
-  }
-  
-  else if (relay == LOW) {
-    relay = HIGH;
-    digitalWrite(23, HIGH);
+  } 
+  else if (relay == HIGH) {
+    relay = LOW;
+    digitalWrite(23, LOW);
   }
 }
